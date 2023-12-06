637a522d5aff169577b4eea2c6d58f96c1e8df93
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-06 15:24:47 -0600

    Update print.c

--- print.c
+++ print.c
@@ -1,5 +1,14 @@
 #include "shell.h"
 
-void print(const char *message) {
+/**
+ * print - Print a message to the standard output
+ * @message: The message to be printed
+ *
+ * Description: This function uses the write system call
+ * to print the specified message to the standard output.
+ */
+
+void print(const char *message)
+{
     write(STDOUT_FILENO, message, strlen(message));
 }
