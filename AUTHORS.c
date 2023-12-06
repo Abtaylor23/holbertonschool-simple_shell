efdb9c0489d76c51aef6245c344530f4f3d96ee4
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-06 15:48:32 -0600

    Update AUTHORS.c

--- AUTHORS.c
+++ AUTHORS.c
@@ -1,2 +1,2 @@
-// This file lists all contributors to the repository.
-// Adrian Taylor 7239@holbertonstudents.com
+/* This file lists all contributors to the repository. */
+/* Adrian Taylor 7239@holbertonstudents.com */
