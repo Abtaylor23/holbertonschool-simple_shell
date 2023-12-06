5f78cd4e546f1982b1ae8759e132a8deb1f7533a
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-06 14:59:52 -0600

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,19 +1,30 @@
 #include "shell.h"
 
-void execute_command(const char *command) {
-    pid_t child_pid = fork();
+/**
+ * execute_command - Execute a command in a new process
+ * @command: The command to be executed
+ */
+void execute_command(const char *command)
+{
+	pid_t child_pid = fork();
 
-    if (child_pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (child_pid == 0) {
-        // Child process
-        execlp(command, command, (char *)NULL);
-        perror("execlp");
-        exit(EXIT_FAILURE);
-    } else {
-        // Parent process
-        wait(NULL);
-    }
+	if (child_pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (child_pid == 0)
+	{
+		/* Child process */
+		execlp(command, command, (char *)NULL);
+		perror("execlp");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		/* Parent process */
+		wait(NULL);
+	}
 }
+
 
