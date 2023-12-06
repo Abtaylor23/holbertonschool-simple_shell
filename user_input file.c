d95e92aff9d8d52d0de089cb3ab0b8849993f9c1
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-06 15:31:39 -0600

    Update user_input file.c

--- user_input file.c
+++ user_input file.c
@@ -10,17 +10,19 @@
  * It handles potential errors during input and removes the
  * trailing newline character.
  */
-void read_command(char *command, size_t size) {
+void read_command(char *command, size_t size)
+{
     
-if (fgets(command, size, stdin) == NULL) {
-    if (feof(stdin)) {
-        printf("\n");
-        exit(EXIT_SUCCESS);
-    else 
+if (fgets(command, size, stdin) == NULL)
+{
+if (feof(stdin)) {
+    printf("\n");
+    exit(EXIT_SUCCESS);
+else 
     {
-        print("Error while reading input.\n");
-        exit(EXIT_FAILURE);
-            }
-        }
+    print("Error while reading input.\n");
+    exit(EXIT_FAILURE);
+    }
+    }
     command[strcspn(command, "\n")] = '\0'; // Remove newline
 }
