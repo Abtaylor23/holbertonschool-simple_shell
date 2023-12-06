7ae4ad8e7c3421df082575698171e84c4c02c5c7
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-06 15:41:08 -0600

    Update user_input file.c

--- user_input file.c
+++ user_input file.c
@@ -1,7 +1,10 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * user_input - Read user input from the standard input
+ * read_command - Read user input from the standard input
  * @command: The array to store the user input
  * @size: The size of the command array
  *
@@ -12,17 +15,15 @@
  */
 void read_command(char *command, size_t size)
 {
-    
-if (fgets(command, size, stdin) == NULL)
-{
-if (feof(stdin)) {
-    printf("\n");
-    exit(EXIT_SUCCESS);
-else 
+    if (fgets(command, size, stdin) == NULL)
     {
-    print("Error while reading input.\n");
-    exit(EXIT_FAILURE);
-    }
+        if (feof(stdin)) {
+            printf("\n");
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Error while reading input.\n");
+            exit(EXIT_FAILURE);
+        }
     }
     command[strcspn(command, "\n")] = '\0'; // Remove newline
 }
