23035caa4034b9affd22f7ad8f1badab69b40759
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-06 14:48:45 -0600

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,14 +1,15 @@
 #include "shell.h"
 
-void execute_command(const char *command) 
+void execute_command(const char *command)
 {
-    pid_t child_pid = fork();
+pid_t child_pid = fork();
 
-    if (child_pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (child_pid == 0) {
-        // Child process
+if (child_pid == -1) {
+    perror("fork");
+    exit(EXIT_FAILURE);
+} else if (child_pid == 0)
+{
+
         execlp(command, command, (char *)NULL);
         perror("execlp");
         exit(EXIT_FAILURE);
