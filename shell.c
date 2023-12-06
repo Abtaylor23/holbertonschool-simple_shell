af0337d69d96a82a5a0363404f83887fe912ec29
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-06 15:28:19 -0600

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,15 +1,25 @@
 #include "shell.h"
 
+/**
+ * main - Entry point of the shell program
+ *
+ * Description: This function initializes the shell and
+ * enters an infinite loop to continuously prompt the user
+ * for input, read the input, and execute corresponding commands.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char command[120];
 
-int main(void) {
-    char command[120];
+while (true)
+{
+    display_prompt();
+    read_command(command, sizeof(command));
+    execute_command(command);
+}
 
-    while (true) {
-        //display_prompt();
-        //read_command(command, sizeof(command));
-        //execute_command(command);
-        //}
-
-        return 0;
+    return 0;
     }
 }
