38fe418d5b8e195d87713af7d5dc96884ec97295
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-06 20:48:04 -0600

    changed format

--- main source file.c
+++ main source file.c
@@ -13,10 +13,13 @@
 int main(void)
 {
     char command[128];
-    while (1) {
+
+    while (1)
+    {
         display_prompt();
         user_input(command, sizeof(command));
         execute_command(command);
     }
+
     return 0;
 }
