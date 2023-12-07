7a068bdfe1cac1bd67771e4f4e00283d2c289acc
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-06 18:56:19 -0600

    Update main source file.c

--- main source file.c
+++ main source file.c
@@ -13,7 +13,7 @@ int main(void)
 {
     char command[128];
     while (1) {
-        display_promt();
+        display_prompt();
         user_input(command, sizeof(command));
         execute_command(command);
     }
