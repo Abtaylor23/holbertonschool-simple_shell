3c8d14c70b7227926c706733f5b8918ed505a936
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-07 19:13:21 -0600

    Update find_path.c

--- find_path.c
+++ find_path.c
@@ -1,7 +1,16 @@
 #include "main.h"
 
-char **find_path(char **environ) {
+char **find_path(void) {
     char **some_pointer;
+
+ 
+    some_pointer = (char **)malloc(sizeof(char *) * SIZE); 
+    if (some_pointer == NULL) {
+        
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
 
 
     return some_pointer;
