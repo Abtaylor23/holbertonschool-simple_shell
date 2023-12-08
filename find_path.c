7ec07038a7ab8120c5b426c724f94428388b8d4c
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-07 19:07:34 -0600

    Update find_path.c

--- find_path.c
+++ find_path.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
 char **find_path(char **environ) {
-   
+    char **some_pointer;
+
+    // Your implementation of find_path, allocate memory if necessary
+    // ...
+
     return some_pointer;
 }
