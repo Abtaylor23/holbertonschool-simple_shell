6ea0af2a8a323409ed067ee6ab8b167d3bc7bc2c
Abtaylor23 <144152747+Abtaylor23@users.noreply.github.com>
2023-12-07 19:02:36 -0600

    updated find and read

---
+++ find_path.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+char **find_path(char **environ) {
+   
+    return some_pointer;
+}
---
+++ read_line.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+char *read_line(int *i_eof) {
+   
+    return some_pointer;
+}
